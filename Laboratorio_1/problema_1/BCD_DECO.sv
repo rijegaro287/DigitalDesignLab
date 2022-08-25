1d96aebe94576b63a2da720372e37dd86bf10c90
Ricardo Gatgens <gatgens48@gmail.com>
2022-08-24 23:32:06 -0600

    Decodificador BCD de 4 bits

--- Laboratorio_1/problema_1/BCD_DECO.sv
+++ Laboratorio_1/problema_1/BCD_DECO.sv
@@ -1,9 +1,20 @@
-module BCD_DECO(N, a, b, c, d, e, f, g);
-    input N[3:0];
-    output a, b, c, d, e, f, g;
+module BCD_DECO(NUM, BCD);
+    input [3:0] NUM;
+    output [6:0] BCD; // orden -> abcdefg
 
-    always @(N) begin
-
+    always @(NUM) begin
+        case(NUM)
+            4'b0000: BCD <= 7'b1111110; // 0
+            4'b0001: BCD <= 7'b0110000; // 1
+            4'b0010: BCD <= 7'b1101101; // 2
+            4'b0011: BCD <= 7'b1111001; // 3
+            4'b0100: BCD <= 7'b0110011; // 4
+            4'b0101: BCD <= 7'b1011011; // 5
+            4'b0110: BCD <= 7'b1011111; // 6
+            4'b0111: BCD <= 7'b1110000; // 7
+            4'b1000: BCD <= 7'b1111111; // 8
+            4'b1001: BCD <= 7'b1111011; // 9
+            default: BCD <= 7'b0000001; // -
+        endcase
     end
-
 endmodule
--- Laboratorio_1/problema_1/BCD_DECO_TB.sv
+++ Laboratorio_1/problema_1/BCD_DECO_TB.sv
@@ -1,6 +1,6 @@
 module BCD_DECO_TB();
     reg N[3:0];
-    wire a, b, c, d, e, f, g;
+    wire BCD[6:0];
 
 
 endmodule
