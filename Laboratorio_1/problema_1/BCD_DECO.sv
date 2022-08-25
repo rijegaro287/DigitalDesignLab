d1f768023aee7e9d36a389520a98f092955fb479
Ricardo Gatgens <gatgens48@gmail.com>
2022-08-24 22:40:50 -0600

    Archivos iniciales

---
+++ Laboratorio_1/problema_1/BCD_DECO.sv
@@ -0,0 +1,9 @@
+module BCD_DECO(N, a, b, c, d, e, f, g);
+    input N[3:0];
+    output a, b, c, d, e, f, g;
+
+    always @(N) begin
+
+    end
+
+endmodule
---
+++ Laboratorio_1/problema_1/BCD_DECO_TB.sv
@@ -0,0 +1,6 @@
+module BCD_DECO_TB();
+    reg N[3:0];
+    wire a, b, c, d, e, f, g;
+
+
+endmodule
