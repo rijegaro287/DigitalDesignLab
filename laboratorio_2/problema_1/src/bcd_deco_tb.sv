e952458cf10fcaa4bc1c394ed4540a060062cd0e
Ricardo Gatgens <gatgens48@gmail.com>
2022-09-02 00:24:22 -0600

    Lab 3

---
+++ laboratorio_2/problema_1/src/bcd_deco_tb.sv
@@ -0,0 +1,25 @@
+`timescale 10ns/10ns
+
+module bcd_deco_tb();
+    logic [3:0] NUM;
+    logic [6:0] SEG;
+
+    bcd_deco dut (.NUM(NUM), .SEG(SEG));
+
+    initial begin
+        NUM <= 4'b0000; // 0
+        #10; NUM <= 4'b0001; // 1
+        #10; NUM <= 4'b0010; // 2
+        #10; NUM <= 4'b0011; // 3
+        #10; NUM <= 4'b0100; // 4
+        #10; NUM <= 4'b0101; // 5
+        #10; NUM <= 4'b0110; // 6
+        #10; NUM <= 4'b0111; // 7
+        #10; NUM <= 4'b1000; // 8
+        #10; NUM <= 4'b1001; // 9
+
+        $stop;
+    end
+endmodule
+
+
